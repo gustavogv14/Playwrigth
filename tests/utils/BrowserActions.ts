4536c2842a7b36aaabd473b20031b7f12572b76a
Gustavo Gómez <gustavogomez@192.168.1.5>
2026-09-10 16:36:06 -0500

    Se adiciona nuevo archivo 2

--- tests/utils/BrowserActions.ts
+++ tests/utils/BrowserActions.ts
@@ -3,6 +3,7 @@ import { Page,Locator } from '@playwright/test'
 export class BrowserActions {
 
     static async scrollToElement(locator:Locator):Promise<void>{
+          await locator.waitFor({ state: 'attached', timeout: 90000 });
           await locator.scrollIntoViewIfNeeded();
     }
 }
